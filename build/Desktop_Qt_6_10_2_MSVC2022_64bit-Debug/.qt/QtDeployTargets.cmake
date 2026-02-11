adf553f9fa93d92dcbd5468fb701fccdc4ae9fc0
Jeevanreddy04 <yjeevanreddy630@gmail.com>
2026-02-11 19:26:39 +0530

    Cleanup: Fix .gitignore and remove build artifacts

--- build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appMartinGCS_FILE C:/Users/Jeevan Reddy/Desktop/QtProj-2/build/Desktop_Qt_6_10_2_MSVC2022_64bit-Debug/appMartinGCS.exe)
-set(__QT_DEPLOY_TARGET_appMartinGCS_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appMartinGCS_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6QuickControls2d.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6QuickTemplates2d.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Quickd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6OpenGLd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6QmlMetad.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6QmlWorkerScriptd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6QmlModelsd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Qmld.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Networkd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
